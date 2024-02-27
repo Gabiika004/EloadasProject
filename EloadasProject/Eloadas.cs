8dbd8124bdd93f0f5abd7299fbef74d3a47c6fe9
Garai Gabor <ggabika2000@gmail.com>
2024-02-27 15:49:51 +0100

    Feladat 2/c

--- EloadasProject/Eloadas.cs
+++ EloadasProject/Eloadas.cs
@@ -17,6 +17,88 @@
             this.foglalasok = new bool[sorokSzama, helyekSzama];
         }
 
+        public bool Lefoglal()
+        {
+            bool sikeres = false;
+            int i = 0;
+            while (i < this.foglalasok.GetLength(0) && !sikeres)
+            {
+                int j = 0;
+                while (j < this.foglalasok.GetLength(1) && !sikeres)
+                {
+                    if (!this.foglalasok[i, j])
+                    {
+                        this.foglalasok[i, j] = true;
+                        sikeres = true;
+                    }
+                    j++;
+                }
+                i++;
+            }
+            return sikeres;
+        }
+
+
+        public int SzabadHelyek
+        {
+            get
+            {
+                int db = 0;
+                foreach (bool hely in this.foglalasok)
+                {
+                    if (!hely)
+                    {
+                        db++;
+                    }
+                }
+                return db;
+            }
+        }
+
+        public bool Teli
+        {
+            get
+            {
+                bool teli = true;
+                int i = 0;
+                while (i < this.foglalasok.GetLength(0) && teli)
+                {
+                    int j = 0;
+                    while (j < this.foglalasok.GetLength(1) && teli)
+                    {
+                        if (!this.foglalasok[i, j])
+                        {
+                            teli = false;
+                        }
+                        j++;
+                    }
+                    i++;
+                }
+                return teli;
+            }
+        }
+
+        public bool Foglalt(int sorSzam, int helySzam)
+        {
+            if (sorSzam < 1)
+            {
+                throw new ArgumentException("Pozitív egész számot kell megadni!", nameof(sorSzam));
+            }
+            if (sorSzam > this.foglalasok.GetLength(0))
+            {
+                throw new ArgumentException("A sorszám nem lehet nagyobb, mint a sorok száma", nameof(sorSzam));
+            }
+            if (helySzam < 1)
+            {
+                throw new ArgumentException("Pozitív egész számot kell megadni!", nameof(helySzam));
+            }
+            if (helySzam > this.foglalasok.GetLength(1))
+            {
+                throw new ArgumentException("A helyszám nem lehet nagyobb, mint a helyek száma a sorokban", nameof(helySzam));
+            }
+            return this.foglalasok[sorSzam - 1, helySzam - 1];
+        }
+
 
     }
 }

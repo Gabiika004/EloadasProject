38f34d3aee7c90268f4de2e981f5ec60e037a084
Garai Gabor <ggabika2000@gmail.com>
2024-02-27 15:22:22 +0100

    Feladat 2 a/b

--- EloadasProject/Eloadas.cs
+++ EloadasProject/Eloadas.cs
@@ -2,6 +2,21 @@
 {
     public class Eloadas
     {
+        private bool[,] foglalasok;
+
+        public Eloadas(int sorokSzama, int helyekSzama)
+        {
+            if (sorokSzama < 1)
+            {
+                throw new ArgumentException("Pozitív egész számot kell megadni", nameof(sorokSzama));
+            }
+            if (helyekSzama < 1)
+            {
+                throw new ArgumentException("Pozitív egész számot kell megadni!", nameof(helyekSzama));
+            }
+            this.foglalasok = new bool[sorokSzama, helyekSzama];
+        }
+
 
     }
 }

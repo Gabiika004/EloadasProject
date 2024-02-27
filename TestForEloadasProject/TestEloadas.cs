dc50608a999f0ca05f7142b19d582e69e0e6ea4f
Garai Gabor <ggabika2000@gmail.com>
2024-02-27 15:57:29 +0100

    Create a new project for testing

---
+++ TestForEloadasProject/TestEloadas.cs
@@ -0,0 +1,16 @@
+namespace TestForEloadasProject
+{
+    public class TestEloadas
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
